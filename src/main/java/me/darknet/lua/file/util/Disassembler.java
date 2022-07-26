1b85d95de7440803cd1520060e537f2ee3d8b940
Justus Garbe <gihihoh@gmail.com>
2022-07-26 16:38:45 +0200

    Added disassembler

---
+++ src/main/java/me/darknet/lua/file/util/Disassembler.java
@@ -0,0 +1,40 @@
+package me.darknet.lua.file.util;
+
+import me.darknet.lua.file.function.LuaFunction;
+import me.darknet.lua.file.function.LuaInstructionReader;
+import me.darknet.lua.file.instructions.Instruction;
+
+import java.io.PrintStream;
+import java.util.List;
+
+public class Disassembler {
+
+	LuaFunction function;
+	PrintStream out;
+
+	public Disassembler(LuaFunction function, PrintStream out) {
+		this.function = function;
+		this.out = out;
+	}
+
+	public void disassemble() {
+		out.println("Disassembly: " + function.getSource());
+		disassemble(out, 0, 0);
+	}
+
+	private void disassemble(PrintStream out, int level, int index) {
+		out.println(level == 0 ? "function entry " : ("function " + index + ", depth=" + level));
+		for (int i = 0; i < function.getUpvalues().size(); i++) {
+			out.println("\tupvalue [" + i + "]: " + function.getUpvalues().get(i));
+		}
+		for (Instruction instruction : function.getInstructions()) {
+			out.print("\t");
+			out.println(instruction.toString());
+		}
+		out.println();
+		for (int i = 0; i < function.getProtos().size(); i++) {
+			new Disassembler(function.getProtos().get(i), out).disassemble(out, level + 1, i);
+		}
+	}
+
+}
