163f16840d10bddf9235cdcca782272a6c665a95
Justus Garbe <gihihoh@gmail.com>
2022-07-31 10:44:16 +0200

    Fix NIL not being a constant

--- src/main/java/me/darknet/lua/vm/util/ConstantConversion.java
+++ src/main/java/me/darknet/lua/vm/util/ConstantConversion.java
@@ -4,26 +4,20 @@ import me.darknet.lua.file.constant.BooleanConstant;
 import me.darknet.lua.file.constant.Constant;
 import me.darknet.lua.file.constant.NumberConstant;
 import me.darknet.lua.file.constant.StringConstant;
-import me.darknet.lua.vm.value.BooleanValue;
-import me.darknet.lua.vm.value.NumberValue;
-import me.darknet.lua.vm.value.StringValue;
-import me.darknet.lua.vm.value.Value;
+import me.darknet.lua.vm.value.*;
 
 import static me.darknet.lua.file.constant.ConstantTypes.*;
 
 public class ConstantConversion {
 
 	public static Value toValue(Constant constant) {
-		switch (constant.getType()) {
-			case TBOOLEAN:
-				return new BooleanValue(((BooleanConstant) constant).getValue());
-			case TNUMBER:
-				return new NumberValue(((NumberConstant) constant).getValue());
-			case TSTRING:
-				return new StringValue(((StringConstant) constant).getValue());
-			default:
-				throw new IllegalArgumentException("Unsupported constant type: " + constant.getType());
-		}
+		return switch (constant.getType()) {
+			case TNIL -> NilValue.NIL;
+			case TBOOLEAN -> new BooleanValue(((BooleanConstant) constant).getValue());
+			case TNUMBER -> new NumberValue(((NumberConstant) constant).getValue());
+			case TSTRING -> new StringValue(((StringConstant) constant).getValue());
+			default -> throw new IllegalArgumentException("Unsupported constant type: " + constant.getType());
+		};
 	}
 
 }
