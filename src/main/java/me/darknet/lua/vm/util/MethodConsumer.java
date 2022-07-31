bc2a09f551f4d0153815fb626fa5d7b2a25f61ec
Justus Garbe <gihihoh@gmail.com>
2022-07-31 10:44:32 +0200

    Fix MethodConsumer not being callable

--- src/main/java/me/darknet/lua/vm/util/MethodConsumer.java
+++ src/main/java/me/darknet/lua/vm/util/MethodConsumer.java
@@ -6,16 +6,18 @@ import java.util.function.Consumer;
 public class MethodConsumer<T> implements Consumer<T> {
 
 	public Method method;
+	public Object instance;
 
-	public MethodConsumer(Method method) {
+	public MethodConsumer(Object owner, Method method) {
 		if(method == null) throw new IllegalArgumentException("method cannot be null");
 		if(method.getParameterCount() != 1) throw new IllegalArgumentException("method must have one parameter");
 		this.method = method;
+		this.instance = owner;
 	}
 
 	public void accept(T t) {
 		try {
-			method.invoke(t);
+			method.invoke(instance, t);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
