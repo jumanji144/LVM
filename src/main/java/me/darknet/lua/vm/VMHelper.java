240c5dbb28b6c1d99b867f91fb0e1ed48977f2c0
Justus Garbe <gihihoh@gmail.com>
2022-07-31 10:45:13 +0200

    Streamline context preparation for external functions

--- src/main/java/me/darknet/lua/vm/VMHelper.java
+++ src/main/java/me/darknet/lua/vm/VMHelper.java
@@ -4,6 +4,7 @@ import me.darknet.lua.file.function.LuaFunction;
 import me.darknet.lua.vm.data.Closure;
 import me.darknet.lua.vm.data.Table;
 import me.darknet.lua.vm.execution.ExecutionContext;
+import me.darknet.lua.vm.value.NilValue;
 import me.darknet.lua.vm.value.Value;
 
 public class VMHelper {
@@ -58,6 +59,65 @@ public class VMHelper {
 		} else {
 			closure.getJavaFunction().accept(ctx);
 		}
+	}
+
+	/**
+	 * Prepares a ctx for a function call.
+	 * @param closure closure to prepare
+	 * @param passed how many arguments were passed
+	 * @param arguments actual arguments (also includes varargs)
+	 * @return prepared ctx
+	 */
+	public ExecutionContext prepareCtx(Closure closure, int passed, Value... arguments) {
+		ExecutionContext ctx2;
+
+		// prepare context
+		if(closure.isLuaFunction()) {
+
+			LuaFunction function = closure.getLuaFunction();
+
+			ctx2 = new ExecutionContext(vm, function.getMaxStackSize());
+
+			for (int i = 0; i < function.getNumParams(); i++) {
+				if(i >= arguments.length) ctx2.set(i, NilValue.NIL);
+				else ctx2.set(i, arguments[i]);
+			}
+
+			if(function.isVararg()) {
+				int offset = function.getNumParams() + 1;
+				int numArgs = arguments.length - offset;
+				Value[] args = new Value[numArgs];
+				System.arraycopy(arguments, offset, args, 0, numArgs);
+				ctx2.setVarargs(args);
+			}
+
+			// set up function
+			ctx2.setCurrentFunction(function);
+			ctx2.setCurrentClosure(closure);
+
+
+		} else {
+
+			int args = passed - 1;
+			if(args== -1) { // arguments are varargs
+				// cant know them so just make them all the register we have left
+				args = arguments.length;
+			}
+
+			// create state for function
+			ctx2 = new ExecutionContext(vm, args);
+
+			// copy args to registers
+			for (int i = 0; i < args; i++) {
+				ctx2.set(i, arguments[i]);
+			}
+
+			// execute function
+			ctx2.setPc(-1);
+			ctx2.setCurrentClosure(closure);
+
+		}
+		return ctx2;
 	}
 
 }
