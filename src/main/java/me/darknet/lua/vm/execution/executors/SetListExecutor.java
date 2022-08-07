c64e59f2ab55f41e411d881fe71b168f4a1c34b8
Justus Garbe <gihihoh@gmail.com>
2022-08-07 11:55:06 +0200

    SetList instruction

---
+++ src/main/java/me/darknet/lua/vm/execution/executors/SetListExecutor.java
@@ -0,0 +1,30 @@
+package me.darknet.lua.vm.execution.executors;
+
+import me.darknet.lua.file.instructions.SetListInstruction;
+import me.darknet.lua.vm.execution.ExecutionContext;
+import me.darknet.lua.vm.execution.Executor;
+import me.darknet.lua.vm.value.TableValue;
+import me.darknet.lua.vm.value.Value;
+
+public class SetListExecutor implements Executor<SetListInstruction> {
+
+	@Override
+	public void execute(SetListInstruction inst, ExecutionContext ctx) {
+
+		int amount = inst.getAmount();
+		int offset = inst.getOffset();
+
+		if(amount == 0) {
+			amount = (ctx.getTop() - ctx.reg(inst.getRegister())) - 1;
+		}
+
+		TableValue table = (TableValue) ctx.get(inst.getRegister());
+
+		for (int i = 0; i < amount; i++) {
+			Value value = ctx.get(inst.getRegister() + offset + i);
+			table.getTable().insert(i, value);
+		}
+
+	}
+
+}
