aef066e838b48c09213e8fd40f0e7b7ea2831de6
Justus Garbe <gihihoh@gmail.com>
2022-08-13 16:29:55 +0200

    Global `arg` table

--- src/main/java/me/darknet/lua/vm/VM.java
+++ src/main/java/me/darknet/lua/vm/VM.java
@@ -1,18 +1,24 @@
 package me.darknet.lua.vm;
 
 import lombok.Getter;
+import lombok.Setter;
 import me.darknet.lua.vm.data.Table;
 import me.darknet.lua.vm.library.Libraries;
 import me.darknet.lua.vm.library.Library;
+import me.darknet.lua.vm.value.StringValue;
 import me.darknet.lua.vm.value.TableValue;
 
 @Getter
 public class VM {
 
+	public static final long VM_BOOT_TIME = System.nanoTime();
+
 	VMHelper helper;
 	Interpreter interpreter;
 	Libraries libraries;
 	Table global;
+	@Setter
+	String[] programArguments = new String[0];
 
 	public VM() {
 		interpreter = new Interpreter();
@@ -26,6 +32,14 @@ public class VM {
 		loadLibraries();
 	}
 
+	public Table getArgTable() {
+		Table argTable = new Table();
+		for (int i = 0; i < programArguments.length; i++) {
+			argTable.set(i, new StringValue(programArguments[i]));
+		}
+		return argTable;
+	}
+
 	/**
 	 * Load global and libraries
 	 */
@@ -34,6 +48,7 @@ public class VM {
 		Library top = libraries.getTop(); // top level library (aka base)
 		Table table = top.construct();
 		table.set("_G", new TableValue(table));
+		table.set("arg", new TableValue(getArgTable()));
 		// merge table into global
 		table.merge(global);
 		libraries.getLibraries().forEach((name, library) -> global.set(name, new TableValue(library.construct())));
